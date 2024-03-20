dc3fac8c13f5f12ecb2f34da375d19a508eefe7f
Rationew <julieynjoroge@gmail.com>
2024-03-20 11:01:59 +0300

    input message

---
+++ user_input.py
@@ -0,0 +1,13 @@
+# Ask user for their name
+name = input("What is your name? ")
+
+# Ask user for their age
+age = input("How old are you? ")
+
+# Ask user for their location
+location = input("Where are you located? ")
+
+# Print personalized message
+print(f"Hello {J, you are {29} years old and live in {Silicon valley}.")
+
+
